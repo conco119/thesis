6c984f69a07a1c7d84f2fa32c2633fd510f67566
mactd <uchiha9itachi6@gmail.com>
2018-04-03 13:03:05 +0700

    path windows vs mac

--- configs/config.php
+++ configs/config.php
@@ -1,11 +1,12 @@
 <?php
 
-define('FILE_CONF_DATABASE', 'constant/database.ini');
+define('FILE_CONF_DATABASE', '../../constant/database.ini');
 define('DB_INFO', get_db_info());
 // lay info db
 function get_db_info() {
   if(file_exists(FILE_CONF_DATABASE)){
     $info = parse_ini_file(FILE_CONF_DATABASE);
+    var_dump($info);
     if(count($info) > 1){
       $info[] = @$content['server'];
       $info[] = @$content['data_user'];
